5e7425432faabe3228e863643ae00414bcfef9fa
Andrew Yenalavitch <yenalava@coyote.csusb.edu>
2014-10-15 08:32:22 -0700

    Hello world assignment complete (with lighting change)

---
+++ Hello World/Assets/Scripts/CatClickColor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatClickColor : MonoBehaviour {
+	private Animator anim;
+	private bool isCatClicked;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		isCatClicked = false;
+		anim.SetBool("CatClicked", false); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown() {
+		if (isCatClicked == true) { 
+			isCatClicked = false;
+			anim.SetBool("CatClicked", false); 
+		}
+		else { 
+			isCatClicked = true;
+			anim.SetBool("CatClicked", true); 
+		}
+	}
+
+}
---
+++ Hello World/Assets/Scripts/CatController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		target = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			target += Vector3.left * moveSpeed * Time.deltaTime;
+			target.z = transform.position.z;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			target += Vector3.right * moveSpeed * Time.deltaTime;
+			target.z = transform.position.z;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			target += Vector3.up * moveSpeed * Time.deltaTime;
+			target.z = transform.position.z;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			target += Vector3.down * moveSpeed * Time.deltaTime;
+			target.z = transform.position.z;
+		}
+		if (Input.GetMouseButtonDown(0)) {
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.z = transform.position.z;
+		}
+
+		transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+
+	} 
+}
